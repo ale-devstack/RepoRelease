6ba01cd824174179b6dc64a696637ef9778ad164
Alejandro Hernandez <ahernandez.devstack@gmail.com>
2025-06-20 10:52:45 -0600

    Primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="RepoReleses",                             # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Alejandro Hernandez",                   # Tu nombre
+    author_email="ahernandez.devstack@gmail.com",   # Tu correo electrónico
+    url="https://github.com/ale-devstack/RepoReleses", # URL del proyecto
+)
